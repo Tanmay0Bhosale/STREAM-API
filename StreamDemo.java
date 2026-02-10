f33464e7609c62e80a7a44a885e6032d2c6602f3
Tanmay <tanmaytanvi1234@gmail.com>
2026-02-10 12:47:02 +0530

    [Tanmay] : Added UC 2.1 : Ability to Create a Stream and Iterate to Show Each Element using forEach

---
+++ StreamDemo.java
@@ -0,0 +1,16 @@
+package uc21;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamForEachDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+
+        System.out.println("UC2.1 Output:");
+
+        numbers.stream().forEach(System.out::println);
+    }
+}
